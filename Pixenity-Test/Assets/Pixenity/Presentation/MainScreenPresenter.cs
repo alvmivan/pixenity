b52137d30bb2ebc76b5e57c3600bc9342bcc2bf3
Marcos Alvarez <31220059+alvmivan@users.noreply.github.com>
2020-11-24 10:57:15 -0600

    init

---
+++ Pixenity-Test/Assets/Pixenity/Presentation/MainScreenPresenter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Pixenity
+{
+    public class MainScreenPresenter
+    {
+        private readonly MainScreen window;
+        private readonly DrawerService drawerService;
+        private readonly DrawableField<bool> lines; 
+        
+        
+        public MainScreenPresenter(MainScreen window, DrawerService drawerService)
+        {
+            this.window = window;
+            this.drawerService = drawerService;
+            lines = drawerService.CreateBool("Show Lines");
+        }
+
+        public void OnGUI()
+        {
+            lines.Draw();
+        }
+    }
+
+    public interface DrawerService
+    {
+        DrawableField<bool> CreateBool(string label="");
+    }
+
+    public delegate void ValueChanged<in T>(T oldValue, T newValue);
+    public interface DrawableField<T> : IDisposable    
+    {
+        event ValueChanged<T> OnChange ;
+        T Value { get; set; }
+        void Draw();
+    }
+
+    public interface MainScreen
+    {
+        
+    }
+}
